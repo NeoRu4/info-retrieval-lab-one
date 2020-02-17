d5b24d390cbdbc96341f886d5093b666b8e35072
Bystray <bystray@infosysco.ru>
2020-02-17 20:05:22 +0500

    инициализация проекта

---
+++ src/main/java/lab/inforetrieval/InfoRetrievalLabApplication.java
@@ -0,0 +1,13 @@
+package lab.inforetrieval;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfoRetrievalLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfoRetrievalLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lab/inforetrieval/InfoRetrievalLabApplicationTests.java
@@ -0,0 +1,13 @@
+package lab.inforetrieval;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InfoRetrievalLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
